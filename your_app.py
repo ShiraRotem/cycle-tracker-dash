c1cd233b96fe6d68d2a682eeb4a5c638f6e806bb
Shira Rotem <shirarotem@Shiras-MacBook-Air.local>
2024-10-26 15:05:45 +0300

    Fix port binding for Render deployment

--- your_app.py
+++ your_app.py
@@ -11,6 +11,5 @@ app.layout = html.Div(children=[
 
 # Run the server
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    
-app.run_server(debug=True, host='0.0.0.0')
+    app.run_server(debug=True, host='0.0.0.0')
+
